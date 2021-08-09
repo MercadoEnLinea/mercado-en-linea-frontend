298f60d73dc7787332485afe1e81107200c7683a
Jonathan Andrade <jndrdlz@gmail.com>
2021-08-08 21:02:59 -0500

    wip: Componente para busqueda dentro de la barra de navegacion

---
+++ components/Search.jsx
@@ -0,0 +1,14 @@
+import cn from 'classnames'
+import Link from 'next/link'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
+const Search = () => {
+  return (
+    <div>
+      <strong>SSS</strong>
+    </div>
+  )
+}
+
+export default  Search
